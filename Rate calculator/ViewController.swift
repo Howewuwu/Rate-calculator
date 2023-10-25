9c243951a2a7f3e2cfa8e82e16eecff0153ac6ac
Howe <howewu1991@gmail.com>
2023-10-25 19:37:25 +0800

    Initial Commit

---
+++ Rate calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rate calculator
+//
+//  Created by Howe on 2023/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
